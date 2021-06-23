3b713f5d063ea049bbb04e044c09dce3cf4798c7
wills-designs <william3696@live.com>
2021-06-23 17:30:47 -0400

    false value info

--- javaScriptBasics.js
+++ javaScriptBasics.js
@@ -111,6 +111,13 @@ the or operator (||)
 the not operator (!)
 */
 
+/* sidenote about false value, these items will all act as a false value if checked in condition
+- 0
+- Empty strings like "" or ''
+- null which represent when there is no value at all
+- undefined which represent when a declared variable lacks a value
+- NaN, or Not a Number
+*/
 
 
 
