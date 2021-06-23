bce429c834e26e5bbb1b4ab63c27b23676a235dd
Daniel Maton Walker <danielw288@icloud.com>
2021-06-23 11:39:03 +0100

    Added basic operators
    with an example

--- javaScriptBasics.js
+++ javaScriptBasics.js
@@ -3,3 +3,17 @@ console.log('This will print to the console')
 //7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
 // variables can be declared with let or const; let = mutable variable, const = set variable.
 // can access properties and methods by using the ., dot operator.
+
+/* Arithmetic Operators 
+
+    Add: +
+    Subtract: -
+    Multiply: *
+    Divide: /
+    Remainder: %
+
+    This allows you to perform arithmetic in your code
+*/
+  
+console.log(3 + 7) //prints 10 to oonsole
+console.log(5 - 2) //prints 3 to console
