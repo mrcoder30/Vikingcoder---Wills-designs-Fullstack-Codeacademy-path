60f65698fa61e2bb090026e19fda1c3e686b1c30
William <46736580+Wills-Designs@users.noreply.github.com>
2021-06-22 20:39:39 -0400

    Update javaScriptBasics.js
    added in basic notes on data types and variables

--- javaScriptBasics.js
+++ javaScriptBasics.js
@@ -1,1 +1,5 @@
 console.log('This will print to the console')
+
+//7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+// variables can be declared with let or const; let = mutable variable, const = set variable.
+// can access properties and methods by using the ., dot operator.
